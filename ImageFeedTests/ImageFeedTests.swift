5acf958d10f52739b370127d8ef047fc7603ef25
sleepyweepybear <sleepingbearow@gmail.com>
2025-08-23 18:19:01 +0500

    Initial Commit

---
+++ ImageFeedTests/ImageFeedTests.swift
@@ -0,0 +1,17 @@
+//
+//  ImageFeedTests.swift
+//  ImageFeedTests
+//
+//  Created by sleepingbear on 23.08.2025.
+//
+
+import Testing
+@testable import ImageFeed
+
+struct ImageFeedTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
