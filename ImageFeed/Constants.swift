8115612f92591ef3d7e0018672399cb9fec04354
sleepyweepybear <sleepingbearow@gmail.com>
2025-11-27 20:36:41 +0500

    test 10 sprint

---
+++ ImageFeed/Constants.swift
@@ -0,0 +1,8 @@
+import Foundation
+enum Constants {
+    static let accessKey = "<7ARna4q67MDZ3TK9yDEPvAhGQVJlzaV26Z0LitxcG9w>"
+      static let secretKey = "<JyNB9NC9QB9rcsUH6ZQSp9-qTs4usuTM2dxkrKsQTag>"
+      static let redirectURI = "<urn:ietf:wg:oauth:2.0:oob>"
+      static let accessScope = "public+read_user+write_likes"
+      static let defaultBaseURL = URL(string: "https://api.unsplash.com")!
+  }
